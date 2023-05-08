4626cdfc2ea1a5a05af23a614e801ed97f6a27cb
sebasg0 <sebastian.garcia@firstlight.ai>
2023-05-07 20:34:41 -0600

    fix: cypher

--- bt.c
+++ bt.c
@@ -15,46 +15,75 @@
 
 //descifra un texto dado una llave
 void decrypt(long key, char *ciph, int len) {
-
     // Set parity of key
     long k = 0;
-  for(int i=0; i<8; ++i){
-    key <<= 1;
-    k += (key & (0xFE << i*8));
-  }
+    for (int i = 0; i < 8; ++i) {
+        key <<= 1;
+        k += (key & (0xFE << i * 8));
+    }
+
+    // Initialize DES key
     DES_cblock des_key;
     memcpy(des_key, &k, sizeof(k));
     DES_set_odd_parity(&des_key);
-
 
     // Initialize key schedule
     DES_key_schedule key_schedule;
     DES_set_key_unchecked(&des_key, &key_schedule);
 
     // Decrypt the message using ECB mode
-    DES_ecb_encrypt((const_DES_cblock *)ciph, (DES_cblock *)ciph, &key_schedule, DES_DECRYPT);
+    for (size_t i = 0; i < len; i += 8) {
+        DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &key_schedule, DES_DECRYPT);
+    }
+
+    // Remove padding from message
+    size_t pad_len = ciph[len - 1];
+    for (size_t i = len - pad_len; i < len; i++) {
+        if (ciph[i] != pad_len) {
+            // Error: Invalid padding
+            return;
+        }
+    }
+
+    // Null-terminate the plaintext
+    ciph[len - pad_len] = '\0';
+    printf("DECRYPTTTT A ABA JKAKAJH KJAH , %s\n", ciph);
 }
 
 //cifra un texto dado una llave
-void encrypt_h(long key, char *ciph) {
-    // Set parity of key
+void encrypt_h(long key, char *ciph, int len) {
+// Set parity of key
     long k = 0;
-  for(int i=0; i<8; ++i){
-    key <<= 1;
-    k += (key & (0xFE << i*8));
-  }
-  DES_cblock des_key;
-  memcpy(des_key, &k, sizeof(k));
-  DES_set_odd_parity(&des_key);
-  char ciphertext[strlen(ciph)];
+    for(int i=0; i<8; ++i){
+        key <<= 1;
+        k += (key & (0xFE << i*8));
+    }
 
-  // Initialize key schedule
-  DES_key_schedule key_schedule;
-  DES_set_key_unchecked(&des_key, &key_schedule);
+    // Initialize DES key
+    DES_cblock des_key;
+    memcpy(des_key, &k, sizeof(k));
+    DES_set_odd_parity(&des_key);
 
-  // Encrypt the message using ECB mode
-  DES_ecb_encrypt((const_DES_cblock *)ciph, (DES_cblock *)ciphertext, &key_schedule, DES_ENCRYPT);
-  strcpy(ciph, ciphertext);
+    // Initialize key schedule
+    DES_key_schedule key_schedule;
+    DES_set_key_unchecked(&des_key, &key_schedule);
+
+    // Calculate padding length
+    size_t pad_len = 8 - len % 8;
+    if (pad_len == 0) {
+        pad_len = 8;
+    }
+
+    // Add padding to message
+    for (size_t i = len; i < len + pad_len; i++) {
+        ciph[i] = pad_len;
+    }
+
+    // Encrypt the message using ECB mode
+    for (size_t i = 0; i < len + pad_len; i += 8) {
+        DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &key_schedule, DES_ENCRYPT);
+    }
+  // strcpy(ciph, ciphertext);
   printf("AAAAAAAA, %s\n", ciph);
 }
 
@@ -62,8 +91,9 @@ void encrypt_h(long key, char *ciph) {
 char search[] = "donaldo";
 int tryKey(long key, char *ciph, int len){
   char temp[len+1]; //+1 por el caracter terminal
-  memcpy(temp, ciph, len);
-  temp[len]=0;	//caracter terminal
+  strcpy(temp, ciph);
+  // memcpy(temp, ciph, len);
+  // temp[len]=0;	//caracter terminal
   decrypt(key, temp, len);
   printf("IMPRIMIR %s\n", temp);
   return strstr((char *)temp, search) != NULL;
@@ -93,7 +123,7 @@ int main(int argc, char *argv[]){ //char **argv
   memcpy(cipher, eltexto, ciphlen);
   cipher[ciphlen]=0;
   //printf("Plain text");
-  encrypt_h(the_key, cipher);
+  encrypt_h(the_key, cipher, ciphlen);
   printf("Cipher text: %s\n", cipher);
   //INIT MPI
   MPI_Init(&argc, &argv);
