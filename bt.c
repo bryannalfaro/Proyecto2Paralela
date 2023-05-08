2255ca68ffe68eb7d6bf8fa4a1cb42bd5fe27091
sebasg0 <sebastian.garcia@firstlight.ai>
2023-05-07 21:41:09 -0600

    fix: threads

--- bt.c
+++ bt.c
@@ -36,8 +36,12 @@ void decrypt(long key, char *ciph, int len) {
         DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &key_schedule, DES_DECRYPT);
     }
 
-    // Remove padding from message
+    // Check padding
     size_t pad_len = ciph[len - 1];
+    if (pad_len > 8) {
+        // Error: Invalid padding
+        return;
+    }
     for (size_t i = len - pad_len; i < len; i++) {
         if (ciph[i] != pad_len) {
             // Error: Invalid padding
@@ -47,7 +51,7 @@ void decrypt(long key, char *ciph, int len) {
 
     // Null-terminate the plaintext
     ciph[len - pad_len] = '\0';
-    printf("DECRYPTTTT A ABA JKAKAJH KJAH , %s\n", ciph);
+    // printf("DECRYPTTTT A ABA JKAKAJH KJAH , %s\n", ciph);
 }
 
 //cifra un texto dado una llave
@@ -84,22 +88,22 @@ void encrypt_h(long key, char *ciph, int len) {
         DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &key_schedule, DES_ENCRYPT);
     }
   // strcpy(ciph, ciphertext);
-  printf("AAAAAAAA, %s\n", ciph);
+  // printf("AAAAAAAA, %s\n", ciph);
 }
 
 //palabra clave a buscar en texto descifrado para determinar si se rompio el codigo
-char search[] = "donaldo";
+char search[] = "hola";
 int tryKey(long key, char *ciph, int len){
   char temp[len+1]; //+1 por el caracter terminal
   strcpy(temp, ciph);
   // memcpy(temp, ciph, len);
   // temp[len]=0;	//caracter terminal
   decrypt(key, temp, len);
-  printf("IMPRIMIR %s\n", temp);
+  // printf("IMPRIMIR %s\n", temp);
   return strstr((char *)temp, search) != NULL;
 }
 
-char eltexto[] = "Hola Raul, hola bryann, hola donaldo, ya me quiero ir a dormir";
+char eltexto[] = "Hola Raul, hola bryann, hola donaldo, ya me quiero ir a dormir. Pero en realidad lo que sucede es que ya no quiero hacer este proyecto porque me hace llorar el no poder culminar una tarea tan simple pero a la vez tan filosofica, me hace pensar realmente en mi existencia.";
 
 long the_key = 3L;
 //2^56 / 4 es exactamente 18014398509481983
@@ -109,7 +113,7 @@ long the_key = 3L;
 int main(int argc, char *argv[]){ //char **argv
   //printf("Plain text");
   int N, id;
-  long upper = 400L; //upper bound DES keys 2^56
+  long upper = (1L <<56); //upper bound DES keys 2^56
   long mylower, myupper;
   MPI_Status st;
   MPI_Request req;
@@ -126,22 +130,48 @@ int main(int argc, char *argv[]){ //char **argv
   encrypt_h(the_key, cipher, ciphlen);
   printf("Cipher text: %s\n", cipher);
   //INIT MPI
-  MPI_Init(&argc, &argv);
+  MPI_Init(NULL, NULL);
   MPI_Comm_size(comm, &N);
   MPI_Comm_rank(comm, &id);
-  printf("Process %d of %d\n", id, N);
+  // printf("Process %d of %d\n", id, N);
+  long found = 0L;
+  int ready = 0;
 
-  //si es el id 0 desencriptar el texto
-  if(id==0){
-    //printf("Plain text: %s\n", eltexto);
-    //print cipher
-    //printf("Cipher text inside: %s\n", cipher);
-    // decrypt(the_key, cipher, ciphlen);
-    printf("\nAACipher text----------: %s\n", cipher);
-    printf("TRYING KEY %d \n", tryKey(the_key, cipher, ciphlen));
-    printf("\nAACipher text: %s\n", cipher);
+  //distribuir trabajo de forma naive
+  long range_per_node = upper / N;
+  mylower = range_per_node * id;
+  myupper = range_per_node * (id+1) -1;
+  if(id == N-1){
+    //compensar residuo
+    myupper = upper;
+  }
+  printf("Process %d lower %ld upper %ld\n", id, mylower, myupper);
+
+  //non blocking receive, revisar en el for si alguien ya encontro
+  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+
+  for(long i = mylower; i<myupper; ++i){
+    MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
+    if(ready)
+      break;  //ya encontraron, salir
+
+    if(tryKey(i, cipher, ciphlen)){
+      found = i;
+      printf("Process %d found the key\n", id);
+      for(int node=0; node<N; node++){
+        MPI_Send(&found, 1, MPI_LONG, node, 0, comm); //avisar a otros
+      }
+      break;
+    }
   }
 
+  //wait y luego imprimir el texto
+  if(id==0){
+    MPI_Wait(&req, &st);
+    decrypt(found, cipher, ciphlen);
+    printf("Key = %li\n\n", found);
+    printf("%s\n", cipher);
+  }
   printf("Process %d exiting\n", id);
 
   //FIN entorno MPI
